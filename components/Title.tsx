e13b5c9888458ff31fb2acbffca99f51ef510f28
emanuelefavero <emafaux89@gmail.com>
2024-09-19 02:44:49 +0200

    adjust styling for Title component

--- components/Title.tsx
+++ components/Title.tsx
@@ -12,11 +12,12 @@ const styles = StyleSheet.create({
   titleContainer: {
     width: '100%',
     maxWidth: 360,
+    marginBottom: 40,
   },
 
   title: {
     color: '#FFE4E6',
-    fontSize: 32,
+    fontSize: 48,
     fontWeight: 'bold',
     textAlign: 'center',
   },
