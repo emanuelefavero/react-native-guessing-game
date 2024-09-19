5e2ed1a141c5b0e09f9760c0a04e6f7bed7456a0
emanuelefavero <emafaux89@gmail.com>
2024-09-19 04:19:13 +0200

    add PrimaryButton styling

--- components/PrimaryButton.tsx
+++ components/PrimaryButton.tsx
@@ -1,9 +1,28 @@
-import { View, Text } from 'react-native'
+import { Pressable, Text, StyleSheet } from 'react-native'
 
 export default function PrimaryButton({ children }: { children: string }) {
   return (
-    <View>
-      <Text>{children}</Text>
-    </View>
+    <Pressable
+      style={({ pressed }) => [
+        styles.button,
+        { transform: [{ scale: pressed ? 0.95 : 1 }] },
+      ]}
+    >
+      <Text style={styles.buttonText}>{children}</Text>
+    </Pressable>
   )
 }
+
+const styles = StyleSheet.create({
+  button: {
+    backgroundColor: '#9F1239',
+    borderRadius: 30,
+    paddingVertical: 12,
+    paddingHorizontal: 30,
+  },
+  buttonText: {
+    color: 'white',
+    fontSize: 18,
+    textTransform: 'uppercase',
+  },
+})
