06f088bd6057b79377a80e9ef76b4a6e623aa3b7
emanuelefavero <emafaux89@gmail.com>
2024-09-20 17:48:30 +0200

    Revert "add gameState context"
    This reverts commit eb3a593bbd84c43a9f0d56b1d8523837d7106a14.

--- App.tsx
+++ App.tsx
@@ -4,7 +4,6 @@ import { NavigationContainer } from '@react-navigation/native'
 import { createNativeStackNavigator } from '@react-navigation/native-stack'
 import useLoadFonts from '@/hooks/useLoadFonts'
 import { NumberProvider } from '@/context/NumberContext'
-import { GameStateProvider } from '@/context/GameStateContext'
 import StartGameScreen from '@/screens/StartGameScreen'
 import GameScreen from '@/screens/GameScreen'
 import GameOverScreen from '@/screens/GameOverScreen'
@@ -17,43 +16,43 @@ const screenOptions = {
 }
 
 export default function App() {
+  // TODO Create a game state context with "start", "playing" and "over" as values, remove navigation buttons
   const [gameState, setGameState] = useState('start')
 
   const { loaded, error } = useLoadFonts()
+
   if (!loaded || error) return null // show nothing while loading fonts
 
   return (
-    <GameStateProvider>
-      <NumberProvider>
-        <NavigationContainer>
-          <StatusBar style='auto' translucent />
-          <Stack.Navigator>
-            {gameState === 'start' && (
-              <Stack.Screen
-                name='StartGame'
-                component={StartGameScreen}
-                options={{ ...screenOptions, title: 'Start Game' }}
-              />
-            )}
+    <NumberProvider>
+      <NavigationContainer>
+        <StatusBar style='auto' translucent />
+        <Stack.Navigator>
+          {gameState === 'start' && (
+            <Stack.Screen
+              name='StartGame'
+              component={StartGameScreen}
+              options={{ ...screenOptions, title: 'Start Game' }}
+            />
+          )}
 
-            {gameState === 'playing' && (
-              <Stack.Screen
-                name='Game'
-                component={GameScreen}
-                options={{ ...screenOptions, title: 'Game' }}
-              />
-            )}
+          {gameState === 'playing' && (
+            <Stack.Screen
+              name='Game'
+              component={GameScreen}
+              options={{ ...screenOptions, title: 'Game' }}
+            />
+          )}
 
-            {gameState === 'over' && (
-              <Stack.Screen
-                name='GameOver'
-                component={GameOverScreen}
-                options={{ ...screenOptions, title: 'Game Over' }}
-              />
-            )}
-          </Stack.Navigator>
-        </NavigationContainer>
-      </NumberProvider>
-    </GameStateProvider>
+          {gameState === 'over' && (
+            <Stack.Screen
+              name='GameOver'
+              component={GameOverScreen}
+              options={{ ...screenOptions, title: 'Game Over' }}
+            />
+          )}
+        </Stack.Navigator>
+      </NavigationContainer>
+    </NumberProvider>
   )
 }
