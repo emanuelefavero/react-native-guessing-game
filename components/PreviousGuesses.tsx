3574b8fe0c115a9b5f4366978bc4f57ac1ce1327
emanuelefavero <emafaux89@gmail.com>
2024-09-23 07:50:27 +0200

    add ScrollCard component

--- components/PreviousGuesses.tsx
+++ components/PreviousGuesses.tsx
@@ -1,4 +1,5 @@
-import { View, ScrollView, Text, StyleSheet } from 'react-native'
+import { View, StyleSheet } from 'react-native'
+import ScrollCard from './ScrollCard'
 import PreviousGuessesTitle from './PreviousGuessesTitle'
 import PreviousGuess from './PreviousGuess'
 
@@ -9,28 +10,18 @@ export default function PreviousGuesses() {
   ]
 
   return (
-    <ScrollView style={styles.card} alwaysBounceVertical={false}>
+    <ScrollCard>
       <PreviousGuessesTitle />
       <View style={styles.container}>
         {previousGuesses.map((item) => (
           <PreviousGuess key={item.id} item={item} />
         ))}
       </View>
-    </ScrollView>
+    </ScrollCard>
   )
 }
 
 const styles = StyleSheet.create({
-  card: {
-    width: '100%',
-    backgroundColor: 'rgba(76, 5, 25, 0.1)',
-    borderColor: 'rgba(76, 5, 25, 0.2)',
-    borderWidth: 1.2,
-    borderRadius: 16,
-    paddingVertical: 40,
-    marginBottom: 12,
-  },
-
   container: {
     flexDirection: 'row',
     flexWrap: 'wrap',
---
+++ components/ScrollCard.tsx
@@ -0,0 +1,22 @@
+import { ReactNode } from 'react'
+import { View, ScrollView, StyleSheet } from 'react-native'
+
+export default function ScrollCard({ children }: { children: ReactNode }) {
+  return (
+    <ScrollView style={styles.card} alwaysBounceVertical={false}>
+      {children}
+    </ScrollView>
+  )
+}
+
+const styles = StyleSheet.create({
+  card: {
+    width: '100%',
+    backgroundColor: 'rgba(76, 5, 25, 0.1)',
+    borderColor: 'rgba(76, 5, 25, 0.2)',
+    borderWidth: 1.2,
+    borderRadius: 16,
+    paddingVertical: 40,
+    marginBottom: 12,
+  },
+})
