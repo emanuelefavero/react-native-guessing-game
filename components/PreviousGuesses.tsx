1485a3b89d905090f7deeee3dc00478af9b49187
emanuelefavero <emafaux89@gmail.com>
2024-09-23 06:04:33 +0200

    move previous guesses card to own component

--- components/PreviousGuesses.tsx
+++ components/PreviousGuesses.tsx
@@ -1,27 +1,18 @@
 import React from 'react'
-import { ScrollView, Text, StyleSheet } from 'react-native'
+import { Text, StyleSheet } from 'react-native'
+import ScrollCard from '@/components/ScrollCard'
 
 export default function PreviousGuesses() {
   return (
-    <ScrollView style={styles.previousGuesses} alwaysBounceVertical={false}>
+    <ScrollCard>
       <Text style={styles.previousGuessesTitle}>
         Guesses: <Text style={styles.numberOfGuesses}>3</Text>
       </Text>
-    </ScrollView>
+    </ScrollCard>
   )
 }
 
 const styles = StyleSheet.create({
-  previousGuesses: {
-    width: '100%',
-    backgroundColor: 'rgba(76, 5, 25, 0.1)',
-    borderColor: 'rgba(76, 5, 25, 0.2)',
-    borderWidth: 1.2,
-    borderRadius: 16,
-    paddingVertical: 48,
-    marginBottom: 12,
-  },
-
   previousGuessesTitle: {
     color: '#FFE4E6',
     fontSize: 24,
