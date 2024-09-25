26ed3384a24208b21582d030a9829424bd0a8d56
emanuelefavero <emafaux89@gmail.com>
2024-09-25 11:05:19 +0200

    add handleRestartGame in GameOverScreen

--- screens/GameOverScreen.tsx
+++ screens/GameOverScreen.tsx
@@ -1,16 +1,21 @@
 import { Text, View, Button } from 'react-native'
 import { globalStyles } from '@/styles/globalStyles'
 import { GameOverScreenProps } from '@/types/screens'
+import { useNumberDispatch } from '@/context/NumberContext'
 
 export default function GameOverScreen({ navigation }: GameOverScreenProps) {
+  const numberDispatch = useNumberDispatch()
+
+  const handleRestartGame = () => {
+    numberDispatch({ type: 'reset' })
+    navigation.navigate('StartGame')
+  }
+
   return (
     <View style={globalStyles.screenContainer}>
       <Text>Game Over Screen</Text>
 
-      <Button
-        title='Restart Game'
-        onPress={() => navigation.navigate('StartGame')}
-      />
+      <Button title='Restart Game' onPress={handleRestartGame} />
     </View>
   )
 }
